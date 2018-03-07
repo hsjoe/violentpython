06358da07edc0428a6ee8555498f5f587523400d
thinkpad <512292470@qq.com>
2018-03-07 10:16:13 +0800

    complete

--- scanner/scanner.py
+++ scanner/scanner.py
@@ -3,6 +3,7 @@ Author:joe
 Description: scanner of InterNet servers
 1、模块 argparse(Generic Operation System Services)
 2、获取命令行输入的目标地址和端口 ：Host，Port
+3、-H host -p port
 '''
 
 import argparse
@@ -17,12 +18,15 @@ def get_tgthostandport():
         (Host, Port)
     '''
     parser = argparse.ArgumentParser(description="Get Target Host and Port")
-    parser.add_argument('-H', dest='Host', action='store')
-    parser.add_argument('-p', dest='Port', action='store')
-    print(parser.args())
-    
+    parser.add_argument('-H', dest='Host', help='get host')
+    parser.add_argument('-p', dest='Port', type=int, help='get port')
+    # print(parser.parse_args())
+    address = parser.parse_args()
+    host = address.Host
+    port = address.Port
+    return (host, port)
 
-get_tgthostandport()
 
+ADDRESS = get_tgthostandport()
 
-
+print(ADDRESS)
