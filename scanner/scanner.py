5f9098f2fb7457d722a9c4999c7ef4c55a6d3040
thinkpad <512292470@qq.com>
2018-03-09 16:44:47 +0800

    completed

--- scanner/scanner.py
+++ scanner/scanner.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+
 '''
 Author:joe
 Description: scanner of InterNet servers
@@ -7,6 +9,7 @@ Description: scanner of InterNet servers
 '''
 
 import argparse
+import socket
 
 def get_tgthostandport():
     '''
@@ -29,18 +32,51 @@ def get_tgthostandport():
     return [host, port]
 
 
+def conn_scan(tg_host, tg_port):
+    try:
+        conn_stk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        conn_stk.connect(tg_host, tg_port)
+        conn_stk.send("volient python\r\n")
+        reslut = conn_stk.recv(1000)
+        print('[+]%d is open', tg_port)
+        print('[+]' + str(reslut))
+    except:
+        print('[-]%d closed', tg_port)
+
+
+    
+
+
+'''
 ADDRESS = get_tgthostandport()
 print(ADDRESS)
+'''
 
-'''def port_scan():
-    
+def port_scan():
+    '''
     get the tgt_host_ip tgt_host_name
     Paramters: None
     Returns:
+    '''
+    tg_host = get_tgthostandport()[0]
+    tg_port = get_tgthostandport()[1]
+
+    try:
+        tg_ip = socket.gethostbyname(tg_host)
+    except:
+        print('Can not get target ip by %s', tg_host)
+        return
     
+    print('scan reslut for %s', tg_ip)
+    socket.setdefaulttimeout(1)
+
+    for tgport in tg_port:
+        print('sanning port %d:', tgport)
+        conn_scan(tg_host, tgport)
     
 
-def conn_scan():
-    pass
+port_scan()
 
-'''
+
+
+
