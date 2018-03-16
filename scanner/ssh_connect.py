b3c16e43a4fd4a5e6a3f299c082c4bae777611a8
thinkpad <512292470@qq.com>
2018-03-16 16:25:02 +0800

    add

--- scanner/ssh_connect.py
+++ scanner/ssh_connect.py
@@ -11,24 +11,23 @@ def connect(user, host, password):
     ssh_newkey = 'Are you sure you want to continue connecting'
     connStr = 'ssh ' + user + '@' + host
     child = pexpect.spawn(connStr)
-    ret = child.expect([pexpect.TIMEOUT, ssh_newkey,
-    '[P|p]assword:'])
+    ret = child.expect([pexpect.TIMEOUT, ssh_newkey,'[P|p]assword:'])
 
-if ret == 0:
-    print('[-] Error Connecting')
-    return
+    if ret == 0:
+        print('[-] Error Connecting')
+        return
 
-if ret == 1:
-    child.sendline('yes')
-    ret = child.expect([pexpect.TIMEOUT, '[P|p]assword:'])
-    
-if ret == 0:
-    print('[-] Error Connecting')
-    return
+    if ret == 1:
+        child.sendline('yes')
+        ret = child.expect([pexpect.TIMEOUT, '[P|p]assword:'])
+        
+    if ret == 0:
+        print('[-] Error Connecting')
+        return
 
-child.sendline(password)
-child.expect(PROMPT)
-return child
+    child.sendline(password)
+    child.expect(PROMPT)
+    return child
 
 def main():
     host = 'localhost'
