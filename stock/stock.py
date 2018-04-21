146ec36c5c040e477bbcdfca7fe151e61219cd15
hsjoe <hsjoepy@163.com>
2018-04-21 23:21:59 +0800

    k

--- stock/stock.py
+++ stock/stock.py
@@ -1,6 +1,10 @@
 # coding=utf-8
-
+import sys
+import os
 import tushare as ts
-
+os.system('del 000425.csv')
 df = ts.get_k_data('000425', '2017-01-01', '2018-04-19')
-df.to_csv('c:/Users/IBM/Desktop/000425.csv')
+
+del df['code']
+
+df.to_csv('f:/vpython/000426.csv')
