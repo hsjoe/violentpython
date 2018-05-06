af93208be0c4259617466d8dd2c5fc2892072266
jixiu_pc <512292470@qq.com>
2018-05-06 17:47:05 +0800

    s

--- stock/macd.py
+++ stock/macd.py
@@ -1,29 +1,35 @@
 # coding=utf-8
 import pandas as pd
 import numpy as np
+
+import time
 import sys
 import os
+
 import tushare as ts
 import matplotlib.pyplot as plt
 
-stock_code = input('Please input your stock code ')
-
-file_name = 'F:/violentpython/%s'%stock_code
-del_file_name = 'del %s'%file_name
-os.system(del_file_name)
+stock_code = input('Please input your stock code: ')
+folder = time.strftime(r"%Y-%m-%d_%H-%M-%S",time.localtime())
+os.makedirs(r'%s/%s'%(os.getcwd(),folder))
+file_name = r'%s/%s/%s.csv'%(os.getcwd(),folder,stock_code)
+#file_name = 'F:/violentpython/%s'%(stock_code)
+#del_file_name = 'del %s'%(file_name)
+#os.system(del_file_name)
 
 
 #601231
-df = ts.get_k_data('601231') 
+df = ts.get_k_data(stock_code, start='2016-01-01', end='2018-05-04') 
 df = df.drop('code', axis=1)
+df = df.drop('volume', axis=1)
 
 df['sema'] = pd.Series(df['close']).ewm(span=12).mean()
 df['lema'] = pd.Series(df['close']).ewm(span=26).mean()
 
 df['dif'] = df['sema'] - df['lema']
-df['dea'] = pd.Series(df['dif']).ewm(span=12).mean()
-#df = df.round({'dif':2,'dea':2})
-
+df['dea'] = pd.Series(df['dif']).ewm(span=9).mean()
+df = df.round({'dif':2,'dea':2})
+#df = df.fillna(0, inplace=True)
 df['macd'] = 2*(df['dif']-df['dea'])
 
 #df = df.round({'dif':2,'dea':2})
--- stock/test2.py
+++ stock/test2.py
@@ -1,3 +1,21 @@
-s = input('please input your stock code')
 
-print(s)
+# coding=utf-8
+import pandas as pd
+import numpy as np
+
+import time
+import sys
+import os
+import shutil
+
+import tushare as ts
+import matplotlib.pyplot as plt
+
+stock_code = input('Please input your stock code: ')
+folder = time.strftime(r"%Y-%m-%d_%H-%M-%S",time.localtime())
+os.makedirs(r'%s/%s'%(os.getcwd(),folder))
+file_name = r'%s/%s/%s.csv'%(os.getcwd(),folder,stock_code)
+
+df = ts.get_k_data(stock_code, start='2016-01-01', end='2018-05-04')
+
+df.to_csv(file_name)
